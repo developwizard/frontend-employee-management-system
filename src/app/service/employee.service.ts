04e4d75c9df7739cac00da164accc727b0aa9eaf
developwizard <churkin.it@gmail.com>
2023-05-11 20:09:04 +0300

    Added Employee CRUD methods

--- src/app/service/employee.service.ts
+++ src/app/service/employee.service.ts
@@ -1,6 +1,7 @@
 import {Injectable} from "@angular/core";
 import {HttpClient} from "@angular/common/http";
 import {Employee} from "../model/models";
+import {Observable} from "rxjs";
 
 @Injectable({
   providedIn: 'root'
@@ -11,7 +12,23 @@ export class EmployeeService {
   constructor(private http: HttpClient) {
   }
 
-  getEmployeeList(){
+  getEmployeeList(): Observable<Employee[]> {
     return this.http.get<Employee[]>(this.baseUrl);
+  }
+
+  getEmployeeById(id: number): Observable<Employee> {
+    return this.http.get<Employee>(`${this.baseUrl}/${id}`);
+  }
+
+  addEmployee(employee: Employee): Observable<Employee>{
+    return this.http.post<Employee>(this.baseUrl, employee);
+  }
+
+  updateEmployee(employee: Employee): Observable<Employee>{
+    return this.http.put<Employee>(this.baseUrl, employee);
+  }
+
+  deleteEmployee(id: number): Observable<void> {
+    return this.http.delete<void>(`${this.baseUrl}/${id}`);
   }
 }
