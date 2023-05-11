833fc1706c0a1f10132006170e57ed8597cebf53
developwizard <churkin.it@gmail.com>
2023-05-11 19:40:58 +0300

    Added loading spinner

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {MatTableModule} from "@angular/material/table";
 import {MatButtonModule} from "@angular/material/button";
 import {HttpClientModule} from "@angular/common/http";
+import {MatProgressSpinnerModule} from "@angular/material/progress-spinner";
 
 @NgModule({
   declarations: [
@@ -18,6 +19,7 @@ import {HttpClientModule} from "@angular/common/http";
     BrowserModule,
     HttpClientModule,
     MatButtonModule,
+    MatProgressSpinnerModule,
     MatTableModule
   ],
   providers: [],
--- src/app/employee-list/employee-list.component.ts
+++ src/app/employee-list/employee-list.component.ts
@@ -9,6 +9,7 @@ import {EmployeeService} from "../service/employee.service";
   styleUrls: ['./employee-list.component.css']
 })
 export class EmployeeListComponent implements OnInit {
+  isLoading: boolean = true;
   displayedColumns: string[] = ['firstName', 'lastName', 'emailId', 'actions'];
   dataSource: MatTableDataSource<Employee> = new MatTableDataSource<Employee>();
 
@@ -25,13 +26,7 @@ export class EmployeeListComponent implements OnInit {
   ngOnInit(): void {
     this.employeeService.getEmployeeList().subscribe(employees => {
       this.dataSource.data = employees;
+      this.isLoading = false;
     })
   }
 }
-
-const EMPLOYEE_DATA: Employee[] = [
-  {id: 1, firstName: 'Ivan', lastName: 'Ivanov', emailId: 'ivanov@gmail.com'},
-  {id: 2, firstName: 'Oskar', lastName: 'Oskarov', emailId: 'oskarov@gmail.com'},
-  {id: 3, firstName: 'Oleg', lastName: 'Olegov', emailId: 'olegov@gmail.com'},
-  {id: 4, firstName: 'Tatyana', lastName: 'Tanushina', emailId: 'tanushina@gmail.com'}
-];
